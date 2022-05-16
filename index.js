845658f7ce5d809b8b0130abb5d9e7bde687f5c9
Artlfmj <73163159+Artlfmj@users.noreply.github.com>
2022-05-16 17:14:47 +0200

    Update index.js

--- index.js
+++ index.js
@@ -7,6 +7,11 @@ const port = process.env.PORT
 // Render the html files in the subfolder in public folder
 app.use(express.static('public'));
 
+// Send rip 404 if no route is found
+app.use((req, res) => {
+    res.status(404).send('404: Page not found');
+});
+
 app.listen(port, () => {
     console.log(`Server listening on port ${port}`);
     });
